4e92ea54ef61254e6a4566ab217edf3d823671ba
Christian Anthony Dumanauw <chrisdumanauw@gmail.com>
2025-06-05 17:12:41 +0800

    dev: update Header

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import styles from './Header.module.css';
+import pandaLogo from './panda_with_cord.jpg';
 
 function Header() {
     return (
         <>
             <div className={styles.headerShop}>
-                <h1>shope Name</h1>
+                <img src={pandaLogo} alt="panda logo" />
+                <h1>Sellink</h1>
             </div>
         </>
     );
