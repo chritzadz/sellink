d4369d409aeb62b3d997f8855947a0482d75aab0
Christian Anthony Dumanauw <chrisdumanauw@gmail.com>
2025-06-09 22:26:26 +0800

    dev: set up insets because composable set it to takes 50% of the damn screen padding

--- mobile/app/src/main/java/com/sellink/app/MainActivity.kt
+++ mobile/app/src/main/java/com/sellink/app/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.sellink.app.presentation.navigation.NavigationController
 import com.sellink.app.presentation.screen.HomeScreenPreview
 import com.sellink.app.presentation.theme.simpleTheme
@@ -17,10 +20,12 @@ import com.sellink.app.presentation.theme.simpleTheme
 class MainActivity: ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState);
-        enableEdgeToEdge();
         setContent {
             simpleTheme {
-                Scaffold (modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold (
+                    modifier = Modifier.fillMaxSize(),
+                    contentWindowInsets = WindowInsets(0.dp),
+                ) { innerPadding ->
                     NavigationController(
                         modifier = Modifier
                             .fillMaxSize()
--- mobile/app/src/main/java/com/sellink/app/presentation/screen/OrderScreen.kt
+++ mobile/app/src/main/java/com/sellink/app/presentation/screen/OrderScreen.kt
@@ -6,15 +6,23 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
+import androidx.compose.material3.ScaffoldDefaults.contentWindowInsets
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,6 +37,7 @@ import com.sellink.app.domain.models.Good
 import com.sellink.app.domain.models.Order
 import com.sellink.app.presentation.component.OrderList
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OrderScreen(
     modifier: Modifier = Modifier,
@@ -46,7 +55,8 @@ fun OrderScreen(
     )
 
     Scaffold(
-        modifier = Modifier,
+        modifier = Modifier
+            .fillMaxSize(),
         bottomBar = {
             CustomNavBar(
                 SelectedNavItem.OrderRoute,
@@ -54,18 +64,23 @@ fun OrderScreen(
                 onHomeNavClick,
                 onStoreNavClick
             )
-        }
+        },
+        contentWindowInsets = WindowInsets(0.dp)
     ) { innerPadding ->
         Column( //Screen
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(innerPadding)
+                .background(Color.Green)
         ) {
-            Box( //Upper Navigation (Ongoing and Completed)
+            Box( //Spacing for notification
                 modifier = Modifier
                     .height(50.dp)
-                    .background(Color.Green)
-                    .padding(top = 10.dp)
+                    .background(Color.Red)
+            )
+            Box( //Upper Navigation (Ongoing and Completed)
+                modifier = Modifier
+                    .height(40.dp)
             ){
                 Row( //Ongoing and Completed horizontal layout
                     modifier = Modifier
@@ -74,8 +89,7 @@ fun OrderScreen(
                     Box( //Ongoing Box
                         modifier = Modifier
                             .weight(1f)
-                            .height(25.dp)
-                            .background(Color.Blue)
+                            .height(40.dp)
                     ) {
                         Text(
                             text = "Ongoing",
@@ -86,8 +100,7 @@ fun OrderScreen(
                     Box( //Completed Box
                         modifier = Modifier
                             .weight(1f)
-                            .height(25.dp)
-                            .background(Color.Red)
+                            .height(40.dp)
                     ) {
                         Text(
                             text = "Completed",
